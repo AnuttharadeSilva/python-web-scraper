638889fd11e4c0b8e4ec42a9514ada01c9f57f20
anutthara <desilvaanu96@gmail.com>
2024-05-02 17:04:31 +1000

    Added open video project html scripts

--- scrape.py
+++ scrape.py
@@ -3,11 +3,11 @@ import requests
 import re
 import csv
 
-csv_file = open('ovp_1min_mp4.csv', 'w')
+csv_file = open('ovp_all_all.csv', 'w')
 csv_writer = csv.writer(csv_file)
-csv_writer.writerow(['title', 'year', 'description','genre','keywords','duration','popularity','video_url','image_path' ])
+csv_writer.writerow(['title', 'description','genre','keywords','duration','popularity','video_url','image_path' ])
 
-with open('ovp_1min_mp4.html') as html_file:
+with open('ovp_all_all.html') as html_file:
     soup = BeautifulSoup(html_file, 'lxml')
 
 results = soup.find('table', class_='resultsTable')
@@ -16,27 +16,29 @@ for result in results.find_all('tr'):
     td = result.find('td', class_='smalltext')
     td_img = result.find('td', class_='imageTd')
     if td != None and td_img!= None:
-        td_text = td.text
+        td_text = td.text.replace('\n', '').replace('\t', '')
+        # print(td.a.text)
+        title = td.a.text
         video_url = td.find('a', href=True)['href']
         img_file = td_img.find('img', src=True)['src']
 
         # td_text = "Lucky Strike Cigarette Commercial: Square Dance (1948) Lucky Strike cigarette commercial with stop-motion animation of square-dancing cigarettes. Genre: EphemeralKeywords: Advertising: Television commercials;Substance abuse: Tobacco;Animation: Stop-motion;Duration: 00:00:58Popularity (downloads): 5126"
 
-        parts = re.split(r'(\(\d{4}\))|(?:Genre:\s)|(?:Keywords:\s)|(?:Duration:\s)|(?:Popularity\s\(downloads\):\s)', td_text)
+        parts = re.split(r'(?:Genre:\s)|(?:Keywords:\s)|(?:Duration:\s)|(?:Popularity\s\(downloads\):\s)', td_text)
         parts = [x for x in parts if x is not None]
         # print(parts)
 
         try:
-            title = parts[0].strip()
-            year = parts[1].strip('()')
-            description = parts[2].strip()
-            genre = parts[3].strip()
-            keywords = parts[4].strip()
-            duration = parts[5].strip()
-            popularity = parts[6]
+            # title = parts[0].strip()
+            # year = parts[1].strip('()')
+            description = parts[0].strip()
+            genre = parts[1].strip()
+            keywords = parts[2].strip()
+            duration = parts[3].strip()
+            popularity = parts[4]
         except:
-            title = None
-            year = None
+            # title = None
+            # year = None
             description = None
             genre = None
             keywords = None
@@ -44,7 +46,7 @@ for result in results.find_all('tr'):
             popularity = None
 
         print("Title:", title)
-        print("Year:", year)
+        # print("Year:", year)
         print("Description:", description)
         print("Genre:", genre)
         print("Keywords:", keywords)
@@ -54,6 +56,6 @@ for result in results.find_all('tr'):
         print("Image Path:",img_file)
         print("\n")
 
-        csv_writer.writerow([title, year, description, genre, keywords, duration, popularity, video_url, img_file])
+        csv_writer.writerow([title, description, genre, keywords, duration, popularity, video_url, img_file])
 
 csv_file.close()
